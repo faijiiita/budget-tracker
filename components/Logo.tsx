db4e17cde2cb20e3002913596545bb00fadbe446
Faiz Ali <faizali@Faizs-MacBook-Pro.local>
2025-03-11 00:34:46 +0530

    Authentication

---
+++ components/Logo.tsx
@@ -0,0 +1,17 @@
+import { PiggyBank } from "lucide-react";
+import Link from "next/link";
+
+const Logo = ({ isMobile = false }: { isMobile?: boolean }) => {
+  return (
+    <Link href={"/"} className="flex items-center gap-2">
+      {!isMobile && (
+        <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
+      )}
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Budget Tracker
+      </p>
+    </Link>
+  );
+};
+
+export default Logo;
